0252ac10393a3fb5380654f18ff92f705e4873d6
Mehdi Benjelloun <mehdi.cisence@gmail.com>
2017-04-20 17:22:55 +0200

    update strstr and strnstr, protected now

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mbenjell <mbenjell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 16:47:18 by mbenjell          #+#    #+#             */
-/*   Updated: 2016/12/05 16:14:37 by mbenjell         ###   ########.fr       */
+/*   Updated: 2017/04/20 17:21:55 by mbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 char		*ft_strnstr(const char *big, const char *little, size_t n)
 {
-	int i;
-
-	i = big[0];
 	if (ft_strlen(little) == 0)
 		return ((char*)big);
 	if (n < ft_strlen(little))
--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: mbenjell <mbenjell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/18 19:17:45 by mbenjell          #+#    #+#             */
-/*   Updated: 2016/12/05 16:14:30 by mbenjell         ###   ########.fr       */
+/*   Updated: 2017/04/20 17:21:27 by mbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 char		*ft_strstr(const char *big, const char *little)
 {
-	int i;
-
-	i = big[0];
 	if (ft_strlen(little) == 0)
 		return ((char*)big);
 	if (ft_strnequ(big, little, ft_strlen(little)))
