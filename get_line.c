1137eef9008e3b2c85f336b46050db27707e59a9
Mehdi Benjelloun <mehdi.cisence@gmail.com>
2017-05-03 16:09:49 +0200

    fix norme

--- get_line.c
+++ get_line.c
@@ -6,13 +6,13 @@
 /*   By: mbenjell <mbenjell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/02 19:19:34 by mbenjell          #+#    #+#             */
-/*   Updated: 2017/05/02 20:16:24 by mbenjell         ###   ########.fr       */
+/*   Updated: 2017/05/03 16:09:13 by mbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int 		new_buff(t_save *s)
+static int	new_buff(t_save *s)
 {
 	free(s->buff_init);
 	if (!(s->buff = (char*)malloc(sizeof(char) * BUFF_SIZE)))
@@ -32,43 +32,66 @@ static int 		new_buff(t_save *s)
 	return (1);
 }
 
-int 		get_line(t_save *s)
+int			begin_read(t_save *s, t_mem **new)
+{
+	int		r;
+
+	if (s->j > MAX_ALLOC)
+		return (-1);
+	if (!(*new = (t_mem*)malloc(sizeof(t_mem))))
+		return (-1);
+	if (!s->nb)
+	{
+		r = new_buff(s);
+		if (r == 0)
+			return (0);
+		if (r == -1)
+			return (-1);
+	}
+	return (1);
+}
+
+int			nsearch(t_save *s, t_mem *new)
+{
+	if (!(s->p = ft_memchr((const void*)(s->buff), SEP, s->nb)))
+	{
+		new->next = s->mem;
+		s->mem = new;
+		(s->mem)->m = (char*)malloc(sizeof(char) * s->nb);
+		ft_memcpy((void*)(s->mem)->m, (const void*)s->buff, s->nb);
+		s->mem->nb = s->nb;
+		s->nb = 0;
+		s->j += s->mem->nb;
+	}
+	else
+	{
+		new->next = s->mem;
+		s->mem = new;
+		(s->mem)->m = (char*)malloc(sizeof(char) * s->nb);
+		ft_memcpy((void*)(s->mem)->m, (const void*)(s->buff), s->p - s->buff);
+		s->mem->nb = s->p - s->buff;
+		s->nb -= s->p - s->buff + 1;
+		s->buff += s->p - s->buff + 1;
+		s->j += s->mem->nb;
+		return (1);
+	}
+	return (0);
+}
+
+int			get_line(t_save *s)
 {
 	t_mem	*new;
-	int		r;
+	int		b;
 
 	while (42)
 	{
-		if (!(new = (t_mem*)malloc(sizeof(t_mem))))
+		b = begin_read(s, &new);
+		if (b == 0)
+			return (0);
+		if (b == -1)
 			return (-1);
-		if (!s->nb)
-		{
-			r = new_buff(s);
-			if (r == 0)
-				return (0);
-			if (r == -1)
-				return (-1);
-		}
-		if (!(s->p = ft_memchr((const void*)(s->buff), SEP, s->nb)))
-		{
-			new->next = s->mem;
-			s->mem = new;
-			(s->mem)->m = (char*)malloc(sizeof(char) * s->nb);
-			ft_memcpy((void*)(s->mem)->m, (const void*)s->buff, s->nb);
-			s->mem->nb = s->nb;
-			s->nb = 0;
-		}
-		else
-		{
-			new->next = s->mem;
-			s->mem = new;
-			(s->mem)->m = (char*)malloc(sizeof(char) * s->nb);
-			ft_memcpy((void*)(s->mem)->m, (const void*)(s->buff), s->p - s->buff);
-			s->mem->nb = s->p - s->buff;
-			s->nb -= s->p - s->buff + 1;
-			s->buff += s->p - s->buff + 1;
+		if (nsearch(s, new) == 1)
 			return (1);
-		}
 	}
 	return (1);
 }
