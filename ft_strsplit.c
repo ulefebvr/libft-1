04f4b421345e929664ed1742e26ab94c010a39dd
Mehdi Benjelloun <mehdi.cisence@gmail.com>
2017-05-17 23:28:42 +0200

    fix leaks - 1 leak + norm to fix

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: mbenjell <mbenjell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/25 15:26:51 by mbenjell          #+#    #+#             */
-/*   Updated: 2016/12/05 20:17:06 by mbenjell         ###   ########.fr       */
+/*   Updated: 2017/05/17 23:27:09 by mbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,18 +62,28 @@ static char		*ft_strctrim(char const *s, char c)
 static void		ft_extend(char **tab, int len, char *chaine, char c)
 {
 	int			i;
+	char		*temp;
 
 	while (len--)
 	{
 		i = 0;
+		temp = chaine;
 		chaine = ft_strctrim((char const*)chaine, c);
+		free(temp);
 		*tab = ft_strsub(chaine, 0, len_first_word(chaine, c) + 1);
+		temp = *tab;
 		*tab = ft_strctrim((char const*)(*tab), c);
+		free(temp);
 		while (len > 0 && (chaine[i] != c && chaine[i] != '\0'))
 			i++;
+		temp = chaine;
 		chaine = ft_strsub(chaine, i, ft_strlen(chaine) - i);
+		free(temp);
 		if (len < 1)
+		{
+			free(chaine);
 			break ;
+		}
 		tab++;
 	}
 }
@@ -84,20 +94,26 @@ char			**ft_strsplit(char const *s, char c)
 	int			len;
 	char		**tab;
 	char		**tab_init;
+	char		*temp;
 
 	if (!s)
 		return (NULL);
 	chaine = ft_memalloc(ft_strlen(s) + 1);
 	if (!chaine)
 		return (NULL);
-	ft_strcpy(chaine, ft_strctrim(s, c));
+	ft_strcpy(chaine, temp = ft_strctrim(s, c));
+	free(temp);
 	len = nb_word(chaine, c);
 	tab = (char**)ft_memalloc(sizeof(char*) * (len + 1));
 	if (!tab)
-		return (NULL);
-	tab_init = tab;
-	if (ft_strlen(ft_strctrim((const char*)chaine, c)) == 0)
 	{
+		free(chaine);
+		return (NULL);
+	}
+	tab_init = tab;
+	if (ft_strlen(temp = ft_strctrim((const char*)chaine, c)) == 0)
+	{
+		free(temp);
 		*tab = NULL;
 		return (tab);
 	}
